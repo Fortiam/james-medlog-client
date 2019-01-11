2af08563da28c68886f62891a503768a9b13a298
James Bell <Noxachron@gmail.com>
2019-01-11 14:01:50 -0500

    added 2 of 3 welcome buttons back

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 import { /*Link,*/ Redirect } from 'react-router-dom';
 import { login } from '../actions/auth';
 import { DEMO_ACCOUNT } from '../config';
-//import LandingText from './landingText';
+import LandingText from './landingText';
 
 export class Welcome extends Component {
     constructor(props){
@@ -43,17 +43,17 @@ export class Welcome extends Component {
            return (<div className='landingBackground'>
                     <div className='imageContainer4'>
                         <div className='image3'></div>
-                        <span className='quick'>Medication schedule aid: MedLog is an app to help parents keep track of their family members' medical records and schedules.</span>
+                        {/* <span className='quick'>Medication schedule aid: MedLog is an app to help parents keep track of their family members' medical records and schedules.</span> */}
                         {/* <img className='image3' src='./rawpixel-600792-unsplash.jpg' alt='pills' /> */}
                     </div>
-                    {/* <div className='imageContainer1'>
+                    <div className='imageContainer1'>
                         <LandingText showMe={this.state.showFirst} onClick={()=>this.flipClick(1)} landing='landingText1' landingPart2='moreContrast' question={'What is Medlog?'} answer={"MedLog is an app to help parents keep track of their family members' medical records and schedules."}/>
-                        <img className='landingImage image1' src='./aerial-aerial-view-application-935869.jpg' alt='medical equipment' />
-                    </div> */}
-                    {/* <div className='imageContainer2'>
-                        <img className='landingImage image2' src='./balance-cobblestone-conceptual-279470.jpg' alt='pills' />
-                    <LandingText showMe={this.state.showSecond} onClick={()=>this.flipClick(2)} landing='landingText2' landingPart2='moreContrast' question={'How?'} answer={'Users create events about medication such as when to be taken, for all the family.'} />
-                    </div> */}
+                        {/* <img className='landingImage image1' src='./aerial-aerial-view-application-935869.jpg' alt='medical equipment' /> */}
+                    </div>
+                    <div className='imageContainer2'>
+                        {/* <img className='landingImage image2' src='./balance-cobblestone-conceptual-279470.jpg' alt='pills' /> */}
+                    <LandingText showMe={this.state.showSecond} onClick={()=>this.flipClick(2)} landing='landingText2' landingPart2='moreContrast' question={'How?'} answer={'Users select medication for each person, and a schedule is automatically created.'} />
+                    </div>
                     {/* <p>After filling out the details of each family member, which medicine they are on, a schedule can be viewed for the entire family.</p> */}
                     {/* <div className='imageContainer3 centerContainer'>
                         <LandingText showMe={this.state.showThird} onClick={()=>this.flipClick(3)} landing='landingText3' landingPart2='moreContrast' question={'Schedule?'} answer={'A calendar view can show everyone\'s appointments or filtered for a single person.'} />
